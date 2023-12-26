2442c196c979d91f57c33f7bd21b6055cb4f5d42
kalman03 <kalman03@163.com>
2023-12-26 23:25:26 +0800

    upgrade to 1.0.2

--- apideploy-jakarta-demos/apideploy-jakarta-swagger3-webflux-demo/src/main/java/com/kalman03/apideploy/swagger3/webflux/demo/Application.java
+++ apideploy-jakarta-demos/apideploy-jakarta-swagger3-webflux-demo/src/main/java/com/kalman03/apideploy/swagger3/webflux/demo/Application.java
@@ -3,13 +3,10 @@ package com.kalman03.apideploy.swagger3.webflux.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.kalman03.apideploy.core.EnableApideploy;
-
 /**
  * @author kalman03
  * @since 2023-08-26
  */
-@EnableApideploy
 @SpringBootApplication(scanBasePackages = { "org.springdoc.demo.app3", "com.kalman03.apideploy.swagger3.webflux.demo" })
 public class Application {
 
